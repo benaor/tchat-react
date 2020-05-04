7be282415fd7bbc3083e6f39a959eff5bbfb3b8d
benaor <benjamingirard25@gmail.com>
2020-05-04 23:25:23 +0200

    add message when user submit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import React, { Component } from 'react'
 import './App.css'
+
 import Formulaire from './components/Formulaire'
 import Message from './components/Message'
+
 class App extends Component {
+  state = {
+    messages: {},
+    pseudo: this.props.match.params.pseudo
+  }
+
+  addMessage = message => {
+    const messages = { ... this.state.messages }
+    messages[`message-${Date.now()}`] = message
+    this.setState({ messages })
+  }
+
   render() {
     return (
       <div className='box'>
         <div>
           <div className="messages">
-
             <Message />
             <Message />
             <Message />
-            <Message />
-            <Message />
-
           </div>
         </div>
-        <Formulaire />
+        <Formulaire
+        pseudo={this.state.pseudo}
+          addMessage={this.addMessage} />
       </div>
     )
   }
--- src/components/Formulaire.js
+++ src/components/Formulaire.js
@@ -1,20 +1,48 @@
 import React, { Component } from "react";
 
 class Formulaire extends Component {
+    state = {
+        message: ''
+    }
+
+    createMessage = () => {
+        const { addMessage, pseudo } = this.props
+
+        const message = {
+            pseudo,
+            message: this.state.message
+        }
+
+        addMessage(message)
+    }
+
+    handleChange = event => {
+        const message = event.target.value
+        this.setState({ message })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        this.createMessage()
+    }
 
     render() {
 
         return (
             <form
+                onSubmit={this.handleSubmit}
                 className="form">
                 <textarea
+                    value={this.state.message}
+                    onChange={this.handleChange}
                     required
-                    maxLength="140" />
+                    maxLength="140"
+                />
                 <div className="info" >
                     140
                   </div>
-                  <button type="submit" >
-                      Envoyer !
+                <button type="submit" >
+                    Envoyer !
                   </button>
             </form>
         )
